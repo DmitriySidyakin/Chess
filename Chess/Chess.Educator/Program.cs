eb861c87d5dba4ea210c8c62ebe15556881fc237
Dmitriy Sidyakin <sidyakind@gmail.com>
2022-11-24 03:06:52 +0300

    updated educator
    updated educator

---
+++ Chess/Chess.Educator/Program.cs
@@ -0,0 +1,23 @@
+﻿MakeEmptyStepFile();
+
+void MakeEmptyStepFile(string fileName = "steps.dat")
+{
+    string directory = string.Format("{0}/data/", AppDomain.CurrentDomain.BaseDirectory);
+    string path = string.Format("{0}{1}", directory, fileName);
+
+    if(!Directory.Exists(directory))
+        Directory.CreateDirectory(directory);
+
+    if (!File.Exists(path))
+    {
+        var stepFile = File.Create(fileName);
+
+        stepFile.Position = 0;
+        
+        for(byte i = 0; i <= byte.MaxValue; i++)
+            stepFile.WriteByte((byte)i);
+
+        stepFile.Close();
+    }
+
+}
