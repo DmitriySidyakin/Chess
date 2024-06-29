51cdfc8bbfad4668b3e18cdfb7d190f1d63e4985
Dmitriy Sidyakin <sidyakind@gmail.com>
2024-06-29 17:18:34 +0300

    Update Middle1.cs

--- Chess/Chess.ComputerPlayer/Middle1.cs
+++ Chess/Chess.ComputerPlayer/Middle1.cs
@@ -12,15 +12,40 @@ namespace Chess.ComputerPlayer
     {
         public override string Name => "Middle 1";
 
-        public Middle1(Board board) : base(board) {  }
+        public record QueueRecord(Board board, long boardLevel = 0) { }
 
+        private Queue<QueueRecord> boardsForCheck = new Queue<QueueRecord>();
+
+        private long stepNumber = 0;
+
+        public Middle1(Board board) : base(board) { }
 
         public override Step MakeStep(int deep)
         {
+            stepNumber++;
             //TODO: Сделать давку офицерами и конями. В конце игра загон под мат/(шах и мат).
 
             // В случает отсутствия стратегии сделать простой шаг.
             return base.MakeStep(deep);
+        }
+
+
+        /// <summary>
+        /// Добавляет доску в очередь.
+        /// </summary>
+        /// <param name="board">Доска</param>
+
+        private void AddBoard(Board board, long boardLevel = 0)
+        {
+            boardsForCheck.Enqueue(new QueueRecord(board, boardLevel));
+        }
+
+        /// <summary>
+        /// Очищает очередь.
+        /// </summary>
+        private void ClearQueue()
+        {
+            boardsForCheck.Clear();
         }
     }
 }
